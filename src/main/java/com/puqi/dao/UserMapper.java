c10d9c434d5f2241c55851ee00d97c49226c2c11
limaomei1986 <limaomei1986@gmail.com>
2018-10-16 22:20:53 +0800

    mybatis generate

---
+++ src/main/java/com/puqi/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.puqi.dao;
+
+import com.puqi.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
